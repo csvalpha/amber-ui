36867afda42ea147f3b877d38eba8eb6cb947dc3
Ruben Smit <rubensmit@live.nl>
2019-04-24 22:04:13 +0200

    Add download button for album

--- app/controllers/photo-albums/photo-album/photos/index.js
+++ app/controllers/photo-albums/photo-album/photos/index.js
@@ -1,7 +1,21 @@
 import Controller from '@ember/controller';
 import { sort } from '@ember/object/computed';
+import { dasherize } from '@ember/string';
+import FileSaverMixin from 'ember-cli-file-saver/mixins/file-saver';
+import { inject as service } from '@ember/service';
 
-export default Controller.extend({
+export default Controller.extend(FileSaverMixin, {
+  fetch: service(),
   photoSorting: ['-createdAt'],
-  sortedPhotos: sort('model.photos', 'photoSorting')
+  sortedPhotos: sort('model.photos', 'photoSorting'),
+  actions: {
+    downloadAlbum() {
+      let model = this.get('model');
+      return this.get('fetch').fetch(`/photo_albums/${model.id}/zip`).then(response => {
+        return response.blob().then(blob => {
+          this.saveFileAs(`${moment(model.get('date')).format('YYYY-MM-DD')}_${dasherize(model.get('title'))}.zip`, blob, 'application/octet-stream');
+        });
+      });
+    }
+  }
 });
