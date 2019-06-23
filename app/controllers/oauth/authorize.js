a8cb4bf007a07752ce5273c01a7c2d048e00aaa0
Matthijs Vos <vos1996@gmail.com>
2019-06-23 19:46:12 +0200

    Improve OAUTH

--- app/controllers/oauth/authorize.js
+++ app/controllers/oauth/authorize.js
@@ -1,38 +1,51 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
 
 export default Controller.extend({
-  ajax: service(),
+  fetch: service(),
 
   /* eslint-disable camelcase */
   queryParams: ['client_id', 'redirect_uri', 'response_type', 'state'],
   /* eslint-enable camelcase */
 
+  clientName: computed('client_id', function () {
+    this.get('fetch').fetch(`/oauth/authorize?client_id=${this.get('client_id')}&redirect_uri=${this.get('redirect_uri')}&response_type=${this.get('response_type')}&state=${this.get('state')}`
+    ).then(response => {
+      response.json().then(json => {
+        if (json.status === "redirect") {
+          location.href = json.redirect_uri;
+        } else {
+          this.set('clientName', json.client_name)
+        }
+      });
+    });
+  }),
+
   actions: {
     authorize() {
-      this.get('ajax').post('/oauth/auth_code_redirect_uri', {
-        data: {
+      this.get('fetch').fetch(`/oauth/authorize`, {
+        body: JSON.stringify({
           /* eslint-disable camelcase */
           client_id: this.get('client_id'),
           redirect_uri: this.get('redirect_uri'),
           response_type: this.get('response_type'),
           state: this.get('state')
           /* eslint-enable camelcase */
-        }
-      }).then((result) => {
-        if (result.auth) {
-          const response = JSON.parse(result.auth);
-
-          if (response.error && response.error === 'invalid_client') {
-            this.set('errorMessage', 'Client ID unknown');
-          } else if (response.redirect_uri) {
-            location.href = response.redirect_uri;
+        }),
+        method: 'POST',
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+      }).then(response => {
+        response.json().then(json => {
+          if (json.error) {
+            this.set('errorMessage', json.error_description);
+          } else if (json.redirect_uri) {
+            location.href = json.redirect_uri;
           } else {
             this.set('errorMessage', 'Unknown error');
           }
-        }
-      }).catch((error) => {
-        this.set('errorMessage', error.message);
+        });
+
       });
     }
   }
