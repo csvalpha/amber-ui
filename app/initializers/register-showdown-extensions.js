8434b83e3e185b8321666684e60523a65400ad6e
Wilco <wilcovanbeijnum@gmail.com>
2019-09-19 12:20:23 +0200

    Fix youtube regex

--- app/initializers/register-showdown-extensions.js
+++ app/initializers/register-showdown-extensions.js
@@ -2,12 +2,13 @@ import showdown from 'showdown';
 
 export function initialize() {
   showdown.extension('youtubeEmbed', () => {
-    const youtubeRegex = /.*(\$\(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([\w-]+)\).*/g;
+    const youtubeRegex = /.*(youtu\.|youtube\.).*(\$\(v\/|e\/|u\/\w+\/|embed\/|v=)([\w-]+)\).*/g;
     return [{
       type: 'lang',
       regex: youtubeRegex,
       replace(text) {
-        const url = `https://www.youtube.com/embed/${youtubeRegex.exec(text)[2]}`;
+        const url = `https://www.youtube.com/embed/${youtubeRegex.exec(text)[3]}`;
+        youtubeRegex.lastIndex = 0; // Reset index, see https://stackoverflow.com/questions/4724701/regexp-exec-returns-null-sporadically
         return `<div class="js-video widescreen"><iframe src="${url}" frameborder="0" allowfullscreen></iframe></div>`;
       }
     }];
