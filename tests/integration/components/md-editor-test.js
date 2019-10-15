e287b8d3ca0be057491cf82e997a6610c27b66da
Casper Smits <casper.smits@nedap.com>
2019-10-15 12:49:29 +0200

    Add initial test for MD editor

---
+++ tests/integration/components/md-editor-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | markdown editor', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should render the textarea content', async function(assert) {
+    assert.expect(1);
+
+    this.set('markdownContent', 'This is some text');
+    await render(hbs`{{md-editor content=markdownContent textareaIdentifier='newForumPost'}}`);
+
+    assert.equal(this.element.querySelector('textarea').value, 'This is some text');
+  });
+});
+
