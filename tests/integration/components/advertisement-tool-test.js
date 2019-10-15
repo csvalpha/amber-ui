7810b938a74516a2ef8d32c5372b2d40eeb7d9bd
Casper Smits <casper.smits@nedap.com>
2019-10-15 12:39:02 +0200

    Add integration test for Advertisement tool

---
+++ tests/integration/components/advertisement-tool-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | advertisement tool', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should render all ads', async function(assert) {
+    assert.expect(1);
+
+    await render(hbs`{{advertisement-tool}}`);
+
+    assert.equal(this.element.querySelectorAll('a.advertisement-item').length, 20);
+  });
+});
+
