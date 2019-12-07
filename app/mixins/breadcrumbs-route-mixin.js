d43b443221d31dde9d221b1bec6533adf9b4bdc9
Casper Smits <casper.smits@nedap.com>
2019-12-07 15:51:05 +0100

    Remove mixin

--- app/mixins/breadcrumbs-route-mixin.js
+++
@@ -1,9 +0,0 @@
-import { computed } from '@ember/object';
-import Mixin from '@ember/object/mixin';
-
-export default Mixin.create({
-  breadCrumb: computed('title', function() {
-    return { title: this.title };
-  }),
-  title: null
-});
--- app/routes/application/index.js
+++ app/routes/application/index.js
@@ -1,14 +1,17 @@
 import Route from '@ember/routing/route';
+import { computed } from '@ember/object';
 import AuthorizationRouteMixin from 'alpha-amber/mixins/authorization-route-mixin';
-import BreadcrumbsRouteMixin from 'alpha-amber/mixins/breadcrumbs-route-mixin';
 import { CanMixin } from 'ember-can';
 
 export const IndexRouteUnauthenticated = Route.extend(
   CanMixin,
-  AuthorizationRouteMixin,
-  BreadcrumbsRouteMixin, {
+  AuthorizationRouteMixin, {
     modelName: null,
     pageActions: [],
+
+    breadCrumb: computed('title', function() {
+      return { title: this.title };
+    }),
 
     queryParams: {
       search: {
