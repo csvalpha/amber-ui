68f6993026fea482a6e7130f4d88e2d1235a58af
matthijs <vos1996@gmail.com>
2020-01-25 17:56:19 +0100

    Migrate mail reject

--- app/controllers/mail-moderations/index.js
+++ app/controllers/mail-moderations/index.js
@@ -3,7 +3,6 @@ import Controller from '@ember/controller';
 import FilterableAndSortableMixin from 'alpha-amber/mixins/filterable-and-sortable-mixin';
 
 export default Controller.extend(FilterableAndSortableMixin, {
-  ajax: service(),
   sortedAttribute: 'received_at',
   routeOnEnter: 'mail-moderations.show'
 });
