0e1d7b327614c116d2c1bb2a248049b5dc224bd4
matthijs <vos1996@gmail.com>
2020-01-25 16:05:52 +0100

    Use store instead of ajax for closing-activities.js

--- app/components/tools/closing-activities.js
+++ app/components/tools/closing-activities.js
@@ -1,31 +1,18 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { A } from '@ember/array';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   store: service(),
   ajax: service(),
-  activities: null,
-  loadClosingActivities(context) {
-    const data = {
+  activities: computed(function() {
+    const params = {
       filter: { closing: true },
       // Include form for correct display of form-opened-label
       include: 'form',
       sort: 'form.respond_until'
     };
 
-    return this.ajax.request('/activities', { data }).then(result => {
-      this.store.pushPayload(result);
-      const activities = new A();
-      result.data.forEach(jsonObject => {
-        const activity = this.store.peekRecord('activity', jsonObject.id);
-        activities.push(activity);
-      });
-      context.set('activities', activities);
-    });
-  },
-  init() {
-    this._super(...arguments);
-    this.loadClosingActivities(this);
-  }
+    return this.store.query('activity', params);
+  })
 });
