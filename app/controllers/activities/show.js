cc1ed8a22912a29e9e6e432ba12f03e5926046f1
matthijs <vos1996@gmail.com>
2020-01-26 23:22:19 +0100

    Do not bubble action for form response

--- app/controllers/activities/show.js
+++ app/controllers/activities/show.js
@@ -8,24 +8,5 @@ export default Controller.extend({
   canSubmitResponse: computed('model.form', 'model.form.currentUserCanRespond', function() {
     const form = this.get('model.form');
     return !isNone(form) && form.get('currentUserCanRespond');
-  }),
-  actions: {
-    submitResponse() {
-      const currentUserResponse = this.get('model.currentUserResponse');
-      const form = this.get('model.form');
-      currentUserResponse.saveWithAnswers().then(() => {
-        // The response is the first thing that is saved (in order to save answers), so currently the response is
-        // always 'incomplete'. Furthermore, the form has a field 'amountOfResponses' which should be updated.
-        // We now reload the response and the corresponding form.
-        currentUserResponse.reload();
-        form.reload();
-        this.flashNotice.sendSuccess('Inschrijving opgeslagen');
-      }).catch(error => {
-        this.set('errorMessage', error.message);
-        if (error.payload.errors && error.payload.errors.isAny('source.pointer', '/data/attributes/user')) {
-          this.set('errorMessage', 'Er is al een response gevonden, probeer eerst te refreshen, zie je dit formulier dan nog? Neem dan contact op met de ict-commissie.');
-        }
-      });
-    }
-  }
+  })
 });
