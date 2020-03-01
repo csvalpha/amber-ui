2af3afc932e18345c2ba979687e8b48dd9b0e2e5
matthijs <vos1996@gmail.com>
2020-03-01 16:36:19 +0100

    Remove router actions deprecation (https://deprecations.emberjs.com/v3.x/#toc_deprecate-router-events)

--- app/routes/forum/categories/category/threads/thread/posts/index.js
+++ app/routes/forum/categories/category/threads/thread/posts/index.js
@@ -1,6 +1,6 @@
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
-import { assign } from '@ember/polyfills';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
+import {assign} from '@ember/polyfills';
 import IndexRoute from 'alpha-amber/routes/application/index';
 import PagedModelRouteMixin from 'alpha-amber/mixins/paged-model-route-mixin';
 
@@ -16,7 +16,7 @@ export default IndexRoute.extend(PagedModelRouteMixin, {
     const thread = this.modelFor('forum.categories.category.threads.thread');
     assign(params, {
       paramMapping: this.paramMapping,
-      filter: { thread: thread.id },
+      filter: {thread: thread.id},
       sort: 'created_at'
     });
     const postsPromise = this.findPaged('forum/post', params);
@@ -28,11 +28,11 @@ export default IndexRoute.extend(PagedModelRouteMixin, {
     };
   },
 
-  title: computed('controller.model.thread.title', function() {
+  title: computed('controller.model.thread.title', function () {
     return this.get('controller.model.thread.title');
   }),
 
-  pageActions: computed('controller.model.thread', function() {
+  pageActions: computed('controller.model.thread', function () {
     return [
       {
         link: 'forum.categories.category.threads.thread.edit',
@@ -51,14 +51,18 @@ export default IndexRoute.extend(PagedModelRouteMixin, {
     ];
   }),
 
-  actions: {
-    didTransition() {
+  init() {
+    this._super(...arguments);
+
+    this.router.on('routeDidChange', _ => {
       // Update forumLastRead
       let currentStore = this.storage.getItem('forumLastRead') || '{}';
       currentStore = JSON.parse(currentStore);
       currentStore[this.get('controller.model.thread.id')] = new Date();
       this.storage.setItem('forumLastRead', JSON.stringify(currentStore));
-    },
+    });
+  },
+  actions: {
     refreshModelsAndRedirectToLastPage() {
       // Refresh thread too to update thread.amountOfPosts
       this.get('controller.model.thread').reload();
--- app/routes/oauth/authorize.js
+++ app/routes/oauth/authorize.js
@@ -1,21 +1,25 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject as service } from '@ember/service';
+import {inject as service} from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   fetch: service(),
 
-  actions: {
-    didTransition() {
+  init() {
+    this._super(...arguments);
+
+    this.router.on('routeDidChange', _ => {
+      // Get client id from the API
       const clientId = this.controller.get('client_id');
       const redirectUri = this.controller.get('redirect_uri');
       const responseType = this.controller.get('response_type');
       const state = this.controller.get('state');
-      const scope =  this.controller.get('scope');
+      const scope = this.controller.get('scope');
 
       this.fetch.fetch(`/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&state=${state}&scope=${scope}`
       ).then(response => {
         response.json().then(json => {
+          // If user already authenticated redirect, otherwise show client name
           if (json.status === 'redirect') {
             location.href = json.redirect_uri;
           } else {
@@ -23,7 +27,6 @@ export default Route.extend(AuthenticatedRouteMixin, {
           }
         });
       });
-    }
+    });
   }
-
 });
