7158d4047706811d2e19a9f959a9014035424eaa
matteo <m.bronkhorst-1@student.utwente.nl>
2020-03-01 01:53:53 +0100

    cleared up confusing use of identifiers, also using toFixed Brought some consistency in the naming of variables. Also applied toFixed() instead of difficult to read rounding methods.

--- app/controllers/debit/collections/show.js
+++ app/controllers/debit/collections/show.js
@@ -21,8 +21,8 @@ export default Controller.extend({
       transactionsGroupedByUser.transactions.pushObject(transaction);
       transactionsGroupedByUser.totalTransactionAmount += transaction.get('amount');
     });
-    transactions.forEach(transaction => {
-      transaction.totalTransactionAmount = Math.round(transaction.totalTransactionAmount*100)/100
+    transactions.forEach(transactionsGroupedByUser => {
+      transactionsGroupedByUser.totalTransactionAmount = transactionsGroupedByUser.totalTransactionAmount.toFixed(2);
     });
     return transactions;
   })
