17363bf1bdde46903bcbf4e05351fb9ff1b1c68f
Matthijs Vos <vos1996@gmail.com>
2020-03-11 22:12:39 +0100

    Do not bubble actions to parent (#120)
    * Do not use sendAction in the form components
    
    * Do not bubble action for form response
    
    * Remove sendAction from form new post
    
    * Log ACL to Debug instead of warn to prevent stacktrace
    
    * Linter
    
    * Fix anglebrackets
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/components/form/form-form.js
+++ app/components/form/form-form.js
@@ -23,56 +23,6 @@ export const FormFormComponent = Component.extend({
     },
     addClosedQuestion() {
       this.send('createQuestion', 'form/closed-question', 'radio');
-    },
-    deleteQuestion(question) {
-      question.deleteRecord();
-    },
-    addOption(question) {
-      const position = question.get('sortedOptions.lastObject.position') + 1 || 0;
-      this.store.createRecord('form/closed-question-option', {
-        question,
-        position
-      });
-    },
-    deleteOption(option) {
-      option.deleteRecord();
-    },
-    moveQuestionUp(question) {
-      const form = this.model;
-      const index = form.get('sortedQuestions').indexOf(question);
-      if (index > 0) {
-        const previousQuestion = form.get('sortedQuestions').objectAt(index - 1);
-        this.send('switchPositions', question, previousQuestion);
-      }
-    },
-    moveQuestionDown(question) {
-      const form = this.model;
-      const index = form.get('sortedQuestions').indexOf(question);
-      if (index < (form.get('sortedQuestions.length') - 1)) {
-        const nextQuestion = form.get('sortedQuestions').objectAt(index + 1);
-        this.send('switchPositions', question, nextQuestion);
-      }
-    },
-    moveOptionUp(option, question) {
-      const index = question.get('sortedOptions').indexOf(option);
-      if (index > 0) {
-        const previousOption = question.get('sortedOptions').objectAt(index - 1);
-        this.send('switchPositions', option, previousOption);
-      }
-    },
-    moveOptionDown(option, question) {
-      const index = question.get('sortedOptions').indexOf(option);
-      if (index < (question.get('sortedOptions.length') - 1)) {
-        const nextOption = question.get('sortedOptions').objectAt(index + 1);
-        this.send('switchPositions', option, nextOption);
-      }
-    },
-    switchPositions(first, second) {
-      const firstPosition = first.get('position');
-      const secondPosition = second.get('position');
-
-      first.set('position', secondPosition);
-      second.set('position', firstPosition);
     }
   }
 });
