9e836df7fdd6e555f8797234e26cc62392e2a6ab
Matthijs Vos <vos1996@gmail.com>
2020-03-11 23:15:12 +0100

    Feature/privacy modal (#87)
    * Make basic modal not require submit button
    
    * Make better privacy modal
    
    * Allow bare strigns
    
    * Fix linter
    
    * Fix linter
    
    * Fix indentation
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Wilco van Beijnum <wilcovanbeijnum@gmail.com>
    Co-authored-by: Casper Smits <casper@caspersmits.nl>

--- app/components/privacy-modal.js
+++ app/components/privacy-modal.js
@@ -1,16 +1,51 @@
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import Component from '@ember/component';
+import {
+  PicturePublicationPreferenceTypes,
+  UserDetailsPreferenceTypes
+} from 'alpha-amber/constants';
 
 export default Component.extend({
   session: service(),
   store: service(),
+  fetch: service(),
   model: alias('session.currentUser'),
   isOpen: false,
+  step: 1,
+  maxSteps: 6,
+  errorMessage: null,
+  userDetailsPreferenceTypes: computed(function() {
+    return Object.entries(UserDetailsPreferenceTypes).map(([value, label]) => ({ value, label }));
+  }),
+  picturePublicationPreferenceTypes: computed(function() {
+    return Object.entries(PicturePublicationPreferenceTypes).map(([value, label]) => ({ value, label }));
+  }),
   actions: {
-    save() {
-      this.model.save();
-      this.set('isOpen', false);
+    select(attribute, value) {
+      this.model.set(attribute, value);
+      this.model.save().then(() => {
+        this.send('nextPage');
+      }).catch(error => {
+        this.set('errorMessage', error.errors.map((e) => {
+          return e.detail;
+        }).join(', '));
+      });
+    },
+    nextPage() {
+      this.set('errorMessage', null);
+      if (this.get('step') >= this.get('maxSteps')) {
+        this.set('isOpen', false);
+      } else {
+        this.set('step', this.get('step') + 1);
+      }
+    },
+    allowWebdav() {
+      return this.fetch.fetch(`/users/${this.model.id}/activate_webdav`, { method: 'POST' }).then(() => {
+        this.model.reload();
+        this.send('nextPage');
+      });
     }
   },
   init() {
