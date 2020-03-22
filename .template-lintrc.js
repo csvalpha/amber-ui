a94c348b1697ac1ba5aa0a1af5156a6eae699ce0
matthijs <vos1996@gmail.com>
2020-03-22 16:22:41 +0100

    Disable violated template linters

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -12,7 +12,20 @@ module.exports = {
     'no-implicit-this': false,
     'no-partial': false,
     'require-valid-alt-text': false,
-    'table-groups': false
+    'table-groups': false,
+    'no-action': false,
+    'no-curly-component-invocation': false,
+    'link-rel-noopener': false,
+    'simple-unless': false,
+    'link-href-attributes': false,
+    'no-duplicate-attributes': false,
+    'no-nested-interactive': false,
+    'no-invalid-meta': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-quoteless-attributes': false,
+    'no-negated-condition': false,
+    'no-unused-block-params': false,
   },
   extends: 'octane'
 };
