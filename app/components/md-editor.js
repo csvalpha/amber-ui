509754679f791dc64150115b939306d5788c97e1
matthijs <vos1996@gmail.com>
2020-04-20 09:15:12 +0200

    Fix md editor

--- app/components/md-editor.js
+++ app/components/md-editor.js
@@ -1,23 +1,34 @@
 import Component from '@ember/component';
-import { set, action } from '@ember/object';
+import { set, action, computed } from '@ember/object';
 
 export default class MdEditorComponent extends Component {
   classNames = ['md-editor-and-toolbar'];
   content = '';
   editMode = true;
 
+  @computed('elementId')
+  get textareaId() {
+    return `${this.elementId}-textarea`;
+  }
+
   @action
   applyStyle(option, modalInput = null) {
+    const textarea = document.getElementById(`${this.textareaId}`);
 
-    // TODO make this work on selected text
-    // It is hard to track what the selected text is without jquery
-    // For now we just append the style
-    let style = option.format.replace('$1', '');
+    const { selectionStart, selectionEnd } = textarea;
+    const selection = textarea.value.substring(selectionStart, selectionEnd);
+
+    let styledSelection = option.format.replace('$1', selection);
     if (option.modal) {
-      style = style.replace('$2', modalInput);
+      styledSelection = styledSelection.replace('$2', modalInput);
     }
 
-    set(this, 'content', this.content + style);
+    const styledContent
+      = this.content.substring(0, selectionStart)
+      + styledSelection
+      + this.content.substring(selectionStart + selection.length);
+
+    set(this, 'content', styledContent);
   }
 
 }
