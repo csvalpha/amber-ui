f604a7f7f7a821c75ddb5a77e2ae02fa59225ad2
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2020-05-26 10:30:15 +0200

    Change dynamic videos to static videos

--- app/components/about-alpha.js
+++ app/components/about-alpha.js
@@ -6,7 +6,18 @@ import { htmlSafe } from '@ember/string';
 export default Component.extend({
   classNames: ['about-alpha'],
   store: service(),
-  videos: computed(function() {
-    return this.store.findAll('about-alpha-video');
-  })
+  videos: [
+    {
+      videoId: 'x8tGpVON49Q',
+      title: 'Over C.S.V. Alpha #1 Peter vertelt over de drie V\'s'
+    },
+    {
+      videoId: 'mjAA02SGnaU',
+      title: 'Over C.S.V. Alpha #2 Remco vertelt over kring'
+    },
+    {
+      videoId: 'Z0IIwxAKt6A',
+      title: 'Over C.S.V. Alpha #3 Christine vertelt over commissies'
+    }
+  ]
 });
--- app/models/about-alpha-video.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default Model.extend({
-  videoId: attr('string'),
-  title: attr('string'),
-  thumbnailUrl: attr('string')
-});
