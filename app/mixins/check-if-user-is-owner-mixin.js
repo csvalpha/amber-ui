976ad65694de8362054fb5c1399ae8b202c5ec5b
matthijs <vos1996@gmail.com>
2020-06-10 11:53:23 +0200

    Migrate mixins

--- app/mixins/check-if-user-is-owner-mixin.js
+++ app/mixins/check-if-user-is-owner-mixin.js
@@ -2,12 +2,12 @@ import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
   isOwner(user) {
-    if (user.id === this.get('author.id')) {
+    if (user.id === this.author.id) {
       return true;
     }
 
     return user.get('memberships').then(() => {
-      return user.get('currentMemberships').some(membership => membership.get('group.id') === this.get('group.id'));
+      return user.get('currentMemberships').some(membership => membership.get('group.id') === this.group.id);
     });
   }
 });
--- app/mixins/rollback-model-on-deactivation-route-mixin.js
+++ app/mixins/rollback-model-on-deactivation-route-mixin.js
@@ -8,12 +8,9 @@ import Mixin from '@ember/object/mixin';
 export default Mixin.create({
   deactivate() {
     this._super();
-    const model = this.get('controller.model');
-    if (model) {
-      // Calling `rollbackAttributes()` for a saved record reverts all the `changedAttributes` to their original value.
-      // If the record `isNew` it will be removed from the store.
-      // https://guides.emberjs.com/v2.6.0/models/creating-updating-and-deleting-records/#toc_persisting-records
-      model.rollbackAttributes();
-    }
+    // Calling `rollbackAttributes()` for a saved record reverts all the `changedAttributes` to their original value.
+    // If the record `isNew` it will be removed from the store.
+    // https://guides.emberjs.com/v2.6.0/models/creating-updating-and-deleting-records/#toc_persisting-records
+    this.controller.model?.rollbackAttributes();
   }
 });
