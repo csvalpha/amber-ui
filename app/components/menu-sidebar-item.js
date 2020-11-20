4203e42fffa8680ce1d51a65835c8306dc419c06
wilco375 <wilcovanbeijnum@gmail.com>
2020-11-20 23:31:54 +0100

    Add menu popups

--- app/components/menu-sidebar-item.js
+++ app/components/menu-sidebar-item.js
@@ -12,6 +12,27 @@ const MenuSidebarItemComponent = Component.extend({
 
       this.layoutManager.closeLeftSidebarIfOnMobile();
     }
+  },
+  didRender() {
+    let layoutManager = this.layoutManager;
+    let element = this.get('element');
+    let title = this.get('title');
+    let popup;
+    element.addEventListener('mouseover', () => {
+      if (!layoutManager.leftSideBarExpanded) {
+        popup = document.createElement('div');
+        popup.appendChild(document.createTextNode(title));
+        document.body.appendChild(popup);
+        popup.className = 'menu-sidebar-item-popup';
+        popup.style.top = element.getBoundingClientRect().top + 'px';
+      }
+    })
+    element.addEventListener('mouseout', () => {
+        if (popup) {
+          document.body.removeChild(popup);
+          popup = null;
+        }
+    })
   }
 });
 export default MenuSidebarItemComponent;
