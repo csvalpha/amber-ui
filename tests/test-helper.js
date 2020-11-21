56ffbead3875e455f5292a045ae86a3052dcb529
matthijs <vos1996@gmail.com>
2020-11-21 21:11:54 +0100

    Upgrade ember source

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,6 @@
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import Application from '../app';
+import Application from 'alpha-amber/app';
 
 setApplication(Application.create({ autoboot: false }));
 
