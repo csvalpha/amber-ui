2f802ef988a11abdc0fe0a8c03e0a96a01fa070d
matthijs <vos1996@gmail.com>
2021-02-28 20:05:14 +0100

    Do not use observer in flash notice

--- app/components/flash-notice.js
+++ app/components/flash-notice.js
@@ -1,28 +1,6 @@
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { observer } from '@ember/object';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  flashNotice: service('flash-notice'),
-  flashNoticeVisibility: null,
-  flashNoticeStatus: null,
-  flashNoticeContent: null,
-  flashNoticeIsPermanent: null,
-  flashNoticeVisibilityObserver: observer('flashNotice.visibility', function() {
-    this.set('flashNoticeVisibility', this.flashNotice.visibility);
-    this.set('flashNoticeStatus', this.flashNotice.status);
-    this.set('flashNoticeContent', this.flashNotice.content);
-    this.set('flashNoticeIsPermanent', this.flashNotice.isPermanent);
-  }),
-  actions: {
-    dismissFlashNotice() {
-      this.flashNotice.dismiss();
-    }
-  },
-  init() {
-    this._super();
-
-    // Initialize observers
-    this.flashNotice.visibility;
-  }
-});
+export default class FlashNoticeComponent extends Component {
+  @service flashNotice;
+}
