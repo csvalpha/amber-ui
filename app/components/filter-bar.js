422357e818184323c152a8f5c45c40ffd5f23a74
matthijs <vos1996@gmail.com>
2021-03-05 12:22:58 +0100

    Rollback filter-bar, mixed classic and native doesnt work!

--- app/components/filter-bar.js
+++ app/components/filter-bar.js
@@ -1,19 +1,20 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { debounce } from '@ember/runloop';
 
-export default class FilterBarComponent extends Component {
-  filter = null;
-  classNames = ['d-none d-md-flex']
-  sortedAttribute = null;
-  sortableAttributes = null;
-  sortedAscending = null;
-
-  set filterDebounce(value) {
-    debounce(this, this.setFilter, value, 250);
+export default Component.extend({
+  filter: null,
+  classNames: ['d-none d-md-flex'],
+  sortedAttribute: null,
+  sortableAttributes: null,
+  sortedAscending: null,
+  filterDebounce: computed('', {
+    set(key, value) {
+      debounce(this, this.setFilter, value, 250);
+      return null;
+    }
+  }),
+  setFilter(filter) {
+    this.set('filter', filter);
   }
-
-  setFilter(value) {
-    this.filter = value;
-  }
-}
-
+});
