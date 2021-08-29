9b14cb4feae691870ad413c87d19d29fd9705368
wilco375 <wilcovanbeijnum@gmail.com>
2021-08-29 21:15:01 +0200

    Fix activity consumption list

---
+++ app/components/form/consumption-list.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component';
+
+const ConsumptionListComponent = Component.extend({
+  responses: null,
+  showCheckbox: false,
+  options: null,
+  extraRows: 0,
+  showRobertHofstra: false,
+  additionalEmptyRows: 0
+});
+
+export default ConsumptionListComponent;
