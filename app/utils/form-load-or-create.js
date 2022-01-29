5ac0eff95a7111f5ae9029d781889aa9fbc0bb60
Guido <35309288+guidojw@users.noreply.github.com>
2022-01-29 21:53:02 +0100

    fix(formloadorcreateutil): use promises and get rid of get/set

--- app/utils/form-load-or-create.js
+++ app/utils/form-load-or-create.js
@@ -1,6 +1,4 @@
-import { A } from '@ember/array';
 import { isNone } from '@ember/utils';
-import { hash } from 'rsvp';
 
 export default class FormLoadOrCreateUtil {
   constructor(entity) {
@@ -8,61 +6,55 @@ export default class FormLoadOrCreateUtil {
   }
 
   loadOrCreateCurrentUserResponse(form) {
-    const currentUserResponseId = form.get('currentUserResponseId');
-
+    const { currentUserResponseId } = form;
     if (currentUserResponseId) {
       return this.entity.store.findRecord('form/response', currentUserResponseId);
     }
 
-    const user = this.entity.session.get('currentUser');
+    const user = this.entity.session.currentUser;
     return this.entity.store.createRecord('form/response', { form, user });
   }
 
-  loadOrCreateAnswers(response) {
-    return response.get('form').then(form => {
-      return hash({
-        // First promise: We request the existing answers in the response, grouped by question id
-        groupedAnswers: response.get('groupedAnswersPromise'),
-        // Second promise: To determine the type of the questions, the questions have to be loaded
-        openQuestions: form.get('openQuestions'),
-        closedQuestions: form.get('closedQuestions')
-      }).then(({ groupedAnswers, openQuestions, closedQuestions }) => {
-        openQuestions.forEach(question => {
-          const questionId = question.get('id');
-          const existingAnswers = isNone(groupedAnswers) ? null : groupedAnswers.get(questionId);
-          const answerExist = !isNone(existingAnswers) && existingAnswers.get('length') > 0;
+  async loadOrCreateAnswers(response) {
+    const form = await response.form;
+    const [groupedAnswers, openQuestions, closedQuestions] = await Promise.all([
+      response.groupedAnswersPromise,
+      form.openQuestions,
+      form.closedQuestions
+    ]);
 
-          if (answerExist) {
-            question.set('linkedAnswer', existingAnswers.get('firstObject'));
-          } else {
-            question.set('linkedAnswer', this.entity.store.createRecord('form/open-question-answer', { response, question }));
-          }
-        });
+    openQuestions.forEach(question => {
+      const questionId = question.id;
+      const existingAnswers = isNone(groupedAnswers) ? null : groupedAnswers[questionId];
+      const answerExist = !isNone(existingAnswers) && existingAnswers.length > 0;
 
-        closedQuestions.forEach(question => {
-          const questionId = question.get('id');
-          const existingAnswers = isNone(groupedAnswers) ? null : groupedAnswers.get(questionId);
-          const answerExist = !isNone(existingAnswers) && existingAnswers.get('length') > 0;
-
-          if (question.get('isMultipleChoice')) {
-            if (answerExist) {
-              question.set('linkedAnswers', existingAnswers);
-            } else {
-              question.set('linkedAnswers', A());
-            }
-          } else {
-            // eslint-disable-next-line no-lonely-if
-            if (answerExist) {
-              question.set('linkedAnswer', existingAnswers.get('firstObject'));
-            } else {
-              question.set('linkedAnswer', this.entity.store.createRecord('form/closed-question-answer', { response }));
-            }
-          }
-        });
-
-        // The promise should return the original response
-        return response;
-      });
+      if (answerExist) {
+        question.linkedAnswer = existingAnswers[0];
+      } else {
+        question.linkedAnswer = this.entity.store.createRecord('form/open-question-answer', { response, question });
+      }
     });
+
+    closedQuestions.forEach(question => {
+      const questionId = question.id;
+      const existingAnswers = isNone(groupedAnswers) ? null : groupedAnswers[questionId];
+      const answerExist = !isNone(existingAnswers) && existingAnswers.length > 0;
+
+      if (question.isMultipleChoice) {
+        if (answerExist) {
+          question.linkedAnswers = existingAnswers;
+        } else {
+          question.linkedAnswers = [];
+        }
+      } else {
+        if (answerExist) {
+          question.linkedAnswer = existingAnswers[0];
+        } else {
+          question.linkedAnswer = this.entity.store.createRecord('form/closed-question-answer', { response });
+        }
+      }
+    });
+
+    return response;
   }
 }
