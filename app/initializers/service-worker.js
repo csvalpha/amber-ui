0717a90be76ad172571bb46656c9fdd5f80b0d20
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-02-03 14:41:49 +0100

    Check for testing

--- app/initializers/service-worker.js
+++ app/initializers/service-worker.js
@@ -1,8 +1,12 @@
+import Ember from 'ember';
+import ENV from 'module:ember.Ember.ENV';
+
 export default {
   name: 'service-worker',
 
   initialize() {
-    if ('serviceWorker' in navigator) {
+    let testing = ENV.environment === 'test'; // Do not register service worker in tests
+    if (!testing && 'serviceWorker' in navigator) {
       // Register service worker for PWA
       navigator.serviceWorker.register('sw.js');
     }
