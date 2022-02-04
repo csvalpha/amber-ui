f6685fd54d1994d5c2b45a79bad4de613654bb5e
Wilco <wilcovanbeijnum@gmail.com>
2022-02-04 15:56:08 +0100

    PWA (#449)
    * PWA
    
    * Remove unused svg
    
    * Check for testing
    
    * Fix linting error
    
    * Fix environment import
    
    * Adjust to feedback

---
+++ app/initializers/service-worker.js
@@ -0,0 +1,13 @@
+import ENV from '../config/environment';
+
+export default {
+  name: 'service-worker',
+
+  initialize() {
+    let testing = ENV.environment === 'test'; // Do not register service worker in tests
+    if (!testing && 'serviceWorker' in navigator) {
+      // Register service worker for PWA
+      navigator.serviceWorker.register('sw.js');
+    }
+  }
+};
