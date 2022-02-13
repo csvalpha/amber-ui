685f7a5f0c419eaef91b94b1c4ea1e3b42cb81ac
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-02-14 00:10:48 +0100

    Add tests

---
+++ tests/unit/routes/books/edit-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Route | books/edit', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    const route = this.owner.lookup('route:books/edit');
+    assert.ok(route);
+  });
+});
