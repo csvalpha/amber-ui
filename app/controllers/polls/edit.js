aa244167856ba583fbec90f872a23c564a55261e
Guido <35309288+guidojw@users.noreply.github.com>
2022-02-15 21:30:05 +0100

    refactor: poll form partial

--- app/controllers/polls/edit.js
+++ app/controllers/polls/edit.js
@@ -1,21 +1,25 @@
-import { inject as service } from '@ember/service';
-import { union } from '@ember/object/computed';
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
+import { union } from '@ember/object/computed';
 
-export default Controller.extend({
-  session: service(),
-  store: service(),
-  flashNotice: service('flash-notice'),
-  combinedErrors: union('model.errors', 'model.form.errors'),
-  actions: {
-    submit() {
-      const poll = this.model;
-      poll.saveWithForm().then(savedPoll => {
-        this.transitionToRoute('polls.show', savedPoll.get('id'));
-        this.flashNotice.sendSuccess('Poll opgeslagen!');
-      }).catch(error => {
-        this.set('errorMessage', error.message);
-      });
-    }
+export default class EditPollController extends Controller {
+  @service('flash-notice') flashNotice;
+  @service session;
+  @service store;
+
+  @union('model.errors', 'model.form.errors')
+  combinedErrors;
+
+  @action
+  submit() {
+    const poll = this.model;
+    poll.saveWithForm().then(savedPoll => {
+      this.transitionToRoute('polls.show', savedPoll.get('id'));
+      this.flashNotice.sendSuccess('Poll opgeslagen!');
+    }).catch(error => {
+      this.set('errorMessage', error.message);
+    });
   }
-});
+}
