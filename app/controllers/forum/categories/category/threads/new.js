a1f4e22d00e04c0ddefcfe1d3e4fb03c87c91873
Guido <35309288+guidojw@users.noreply.github.com>
2022-02-15 20:30:47 +0100

    refactor: thread form partial

--- app/controllers/forum/categories/category/threads/new.js
+++ app/controllers/forum/categories/category/threads/new.js
@@ -1,41 +1,45 @@
-import { inject as service } from '@ember/service';
-import { isNone } from '@ember/utils';
 import NewController from 'alpha-amber/controllers/application/new';
+import { action } from '@ember/object';
+import { isNone } from '@ember/utils';
+import { inject as service } from '@ember/service';
 
-export default NewController.extend({
-  flashNotice: service('flash-notice'),
-  successTransitionTarget: 'forum.categories.category.threads.thread',
-  routeIsNew: true,
-  content: '',
-  actions: {
-    onSuccess() {
-      this._super(...arguments);
-      // Reload thread to update thread.amountOfPosts
-      this.model.thread.reload();
-      // Reload category to update category.amountOfThreads
-      this.model.category.reload();
-    },
-    submit() {
-      this.set('errorMessage', null);
-      const { thread } = this.model;
-      const { post } = this.model;
+export default class NewThreadController extends NewController {
+  @service('flash-notice') flashNotice;
 
-      if (isNone(post.get('message'))) {
-        this.set('errorMessage', 'Je moet eerst een bericht aanmaken');
-        return;
-      }
+  successTransitionTarget = 'forum.categories.category.threads.thread';
 
-      if (!isNone(thread)) {
-        thread.save().then(savedModel => {
-          post.save().then(() => {
-            this.send('onSuccess', savedModel);
-          }).catch(error => {
-            this.send('onError', error);
-          });
+  routeIsNew = true;
+  content = '';
+
+  @action
+  onSuccess() {
+    // Reload thread to update thread.amountOfPosts
+    this.model.thread.reload();
+    // Reload category to update category.amountOfThreads
+    this.model.category.reload();
+  }
+
+  @action
+  submit() {
+    this.set('errorMessage', null);
+    const { thread } = this.model;
+    const { post } = this.model;
+
+    if (isNone(post.get('message'))) {
+      this.set('errorMessage', 'Je moet eerst een bericht aanmaken');
+      return;
+    }
+
+    if (!isNone(thread)) {
+      thread.save().then(savedModel => {
+        post.save().then(() => {
+          this.send('onSuccess', savedModel);
         }).catch(error => {
           this.send('onError', error);
         });
-      }
+      }).catch(error => {
+        this.send('onError', error);
+      });
     }
   }
-});
+}
--- app/controllers/forum/categories/category/threads/thread/edit.js
+++ app/controllers/forum/categories/category/threads/thread/edit.js
@@ -1,11 +1,12 @@
 import EditController from 'alpha-amber/controllers/application/edit';
+import { action } from '@ember/object';
 
-export default EditController.extend({
-  successMessage: 'Topic aangepast!',
-  successTransitionTarget: 'forum.categories.category.threads.thread',
-  actions: {
-    submit() {
-      this.send('saveModel', this.model.thread);
-    }
+export default class EditThreadController extends EditController {
+  successMessage= 'Topic aangepast!';
+  successTransitionTarget = 'forum.categories.category.threads.thread';
+
+  @action
+  submit() {
+    this.send('saveModel', this.model.thread);
   }
-});
+}
