da37d1c8951dd0c028c396f558e986cf155a9f5b
Guido de Jong <35309288+guidojw@users.noreply.github.com>
2022-02-16 22:23:58 +0100

    refactor: import htmlSafe from @ember/template (#483)

--- app/components/cards/frontpage-activity-card-small.js
+++ app/components/cards/frontpage-activity-card-small.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { htmlSafe } from '@ember/string';
+import { htmlSafe } from '@ember/template';
 
 const FrontpageActivityCardSmall = Component.extend({
   classNames: ['card', 'frontpage-activity-card-small', 'border-0', 'p-0'],
