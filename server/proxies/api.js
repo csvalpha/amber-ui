df4001959abc75658b107cf3db4b40419bd9a77a
guidojw <35309288+guidojw@users.noreply.github.com>
2022-03-15 02:04:56 +0100

    fix: downgrade ESLint to version in blueprint package

--- server/proxies/api.js
+++ server/proxies/api.js
@@ -7,11 +7,11 @@ module.exports = function (app) {
   // https://github.com/nodejitsu/node-http-proxy
   let proxy = require('http-proxy').createProxyServer({});
 
-  proxy.on('error', function(err, req) {
+  proxy.on('error', function (err, req) {
     console.error(err, req.url);
   });
 
-  app.use(proxyPath, function(req, res, next){
+  app.use(proxyPath, function (req, res, _next){
     proxy.web(req, res, { target: 'http://localhost:3000' });
   });
 };
--- server/proxies/webdav.js
+++ server/proxies/webdav.js
@@ -2,16 +2,16 @@
 
 const proxyPath = '/webdav';
 
-module.exports = function(app) {
+module.exports = function (app) {
   // For options, see:
   // https://github.com/nodejitsu/node-http-proxy
   let proxy = require('http-proxy').createProxyServer({});
 
-  proxy.on('error', function(err, req) {
+  proxy.on('error', function (err, req) {
     console.error(err, req.url);
   });
 
-  app.use(proxyPath, function(req, res, next){
+  app.use(proxyPath, function (req, res, _next){
     req.url = req.originalUrl;
     proxy.web(req, res, { target: 'http://localhost:3000' });
   });
