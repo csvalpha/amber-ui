07bacc16a36b14d548445929310248e19d24386b
matteo <matbronk@gmail.com>
2022-03-16 11:34:49 +0100

    finalized refactor of photo-comments.

--- app/components/photo-albums/photo.js
+++ app/components/photo-albums/photo.js
@@ -1,11 +1,15 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
 
 export default class Photo extends Component {
   @tracked
   showExif = false;
+
+  get showInfo() {
+    return this.args.showInfo ?? true;
+  }
+
   @action
   toggleShowExif() {
     this.showExif = !this.showExif;
