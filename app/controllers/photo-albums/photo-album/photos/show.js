2c1177e6f5388fd8688be4df4975a772e0ca6bf9
Matteo Bronkhorst <32799956+DrumsnChocolate@users.noreply.github.com>
2022-04-06 21:51:28 +0200

    feat: simplify thread & photo navigation and deduplicate photo-comments implementation (#503)

--- app/controllers/photo-albums/photo-album/photos/show.js
+++ app/controllers/photo-albums/photo-album/photos/show.js
@@ -1,18 +1,14 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { Promise } from 'rsvp';
 
 export default Controller.extend({
   session: service(),
-  store: service(),
-  flashNotice: service('flash-notice'),
-  content: null,
-  showExif: false,
   advanceToPhoto(delta) {
     const photos = this.model.photoAlbum.get('photos');
     const length = photos.get('length');
     const index = (photos.indexOf(this.model) + delta + length) % length;
-    return this.transitionToRoute(
+    this.route;
+    return this.replaceRoute(
       'photo-albums.photo-album.photos.show',
       photos.objectAt(index)
     );
@@ -23,35 +19,6 @@ export default Controller.extend({
     },
     goToNextPhoto() {
       return this.advanceToPhoto(1);
-    },
-    submitPhotoComment() {
-      return new Promise((resolve, reject) => {
-        const content = this.content.trim();
-
-        if (content.length > 0) {
-          const photo = this.model;
-          const photoComment = this.store.createRecord('photoComment', {
-            content,
-            photo,
-          });
-
-          photoComment
-            .save()
-            .then(() => {
-              this.flashNotice.sendSuccess('Reactie opgeslagen!');
-              photo.reload(); // Reload for updated Photo#amountOfComments
-              resolve();
-            })
-            .catch(reject);
-        } else {
-          reject();
-        }
-
-        this.set('content', null);
-      });
-    },
-    toggleShowExif() {
-      this.toggleProperty('showExif');
     },
     onSwipe(direction) {
       return this.advanceToPhoto(-direction);
