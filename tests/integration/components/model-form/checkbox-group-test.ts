8939835a5d4b3f94e99035288a5ee5f7f1398dec
guidojw <35309288+guidojw@users.noreply.github.com>
2022-05-11 16:50:07 +0200

    feat: add stub tests

---
+++ tests/integration/components/model-form/checkbox-group-test.ts
@@ -0,0 +1,8 @@
+import { click, render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | checkbox-group', function (hooks) {
+  setupRenderingTest(hooks);
+});
---
+++ tests/integration/components/model-form/checkbox-input-test.ts
@@ -0,0 +1,8 @@
+import { click, render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | checkbox-input-test', function (hooks) {
+  setupRenderingTest(hooks);
+});
