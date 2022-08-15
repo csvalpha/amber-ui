ddd88a035e95b3d3ee387121b8e3737130cde2a4
Ellen-Wittingen <101463776+Ellen-Wittingen@users.noreply.github.com>
2022-08-15 16:56:53 +0200

    Redesign/external website (#594)
    * implemented top-down ordering of activities
    
    * changed color of location in activity cards to white
    
    * changed navbar
    
    * removed static pages navbar
    
    * added text and interest button below logo
    
    * darkened image overlay
    
    * changed potentential-member-section colors and made content a bit wider
    
    * some cleanup
    
    * fixed some scss linting problems
    
    * text under logo smaller to fit on mobile devices
    
    * fixed index-header overlapping on small mobile devices
    
    * fixed top padding on logged in pages
    
    * compacted code fpr setting availableStaticPages in header-nav.js
    
    * fixed linting errors in header-nav.js
    
    * fixed no space after ':' in activities.scss
    
    * fixed top padding on all pages
    
    * reverted col-md-10 back to original in application.hbs
    
    * align societeit button and image left on mobile

--- app/components/header-nav.js
+++ app/components/header-nav.js
@@ -1,6 +1,8 @@
+import { set } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
 
 export default Component.extend({
   tagName: 'nav',
@@ -10,30 +12,46 @@ export default Component.extend({
   media: service(),
   intl: service(),
   localStorage: service(),
+  store: service(),
   router: service(),
   abilities: service(),
-  unAuthenticatedMenuOptions: computed('intl.locale', function () {
-    return [
-      {
-        link: 'articles',
-        title: this.intl.t('mixin.menuItems.articles'),
-        icon: '',
-        canAccess: this.abilities.can('show articles'),
-      },
-      {
-        link: 'photo-albums',
-        title: this.intl.t('mixin.menuItems.photoAlbums'),
-        icon: '',
-        canAccess: this.abilities.can('show photo-albums'),
-      },
-      {
-        link: 'static-pages',
-        title: this.intl.t('mixin.menuItems.staticPages'),
-        icon: '',
-        canAccess: this.abilities.can('show static-pages'),
-      },
-    ];
+  availableStaticPages: tracked(),
+  staticPagesForDropdown: computed('availableStaticPages', function () {
+    const result = {};
+    if (this.availableStaticPages) {
+      Object.entries(this.availableStaticPages).forEach(([id, title]) => {
+        if (!['word-lid', 'word-oud-lid', 'sponsoring'].includes(id)) {
+          result[id] = title;
+        }
+      });
+    }
+    return result;
   }),
+  unAuthenticatedMenuOptions: computed(
+    'intl.locale',
+    'availableStaticPages',
+    function () {
+      let list = [];
+      list = this.addStaticPageOption(list, 'word-lid', 'becomeMember');
+      list = this.addStaticPageOption(list, 'word-oud-lid', 'becomeOldMember');
+      list.push(
+        {
+          link: 'articles',
+          title: this.intl.t('mixin.menuItems.articles'),
+          icon: '',
+          canAccess: this.abilities.can('show articles'),
+        },
+        {
+          link: 'photo-albums',
+          title: this.intl.t('mixin.menuItems.photoAlbums'),
+          icon: '',
+          canAccess: this.abilities.can('show photo-albums'),
+        }
+      );
+      return this.addStaticPageOption(list, 'sponsoring', 'sponsoring');
+    }
+  ),
+
   actions: {
     handleLogoAction() {
       if (this.media.isMobile) {
@@ -61,5 +79,29 @@ export default Component.extend({
         localStorage.setItem('locale', 'nl');
       }
     },
+    setAvailableStaticPages() {
+      if (!this.session.isAuthenticated && !this.media.isMobile) {
+        this.store.findAll('static-page').then((pages) => {
+          // make key-value pairs for all found static pages
+          let newAvailableStaticPages = pages.reduce(
+            (obj, page) => Object.assign(obj, { [page.id]: page.title }),
+            {}
+          );
+          set(this, 'availableStaticPages', newAvailableStaticPages);
+        });
+      }
+    },
+  },
+  addStaticPageOption: function (list, id, localeName) {
+    if (this.availableStaticPages && this.availableStaticPages[id]) {
+      list.push({
+        link: 'static-pages.show',
+        linkArgument: id,
+        title: this.intl.t('component.headerNav.' + localeName),
+        icon: '',
+        canAccess: this.abilities.can('show static-pages'),
+      });
+    }
+    return list;
   },
 });
