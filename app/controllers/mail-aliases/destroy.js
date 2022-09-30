fa693ba5d3f6bbfa6240bcc4b96333180a3e0fca
matteo <matbronk@gmail.com>
2022-09-30 15:47:15 +0200

    more changes

--- app/controllers/mail-aliases/destroy.js
+++ app/controllers/mail-aliases/destroy.js
@@ -2,5 +2,5 @@ import DestroyController from 'amber-ui/controllers/application/destroy';
 
 export default class MailAliasDestroyController extends DestroyController {
   successTransitionTarget = 'mail-aliases.index';
-  // cancelTransitionTarget = 'mail-aliases.index';
+  cancelTransitionTarget = 'mail-aliases.show';
 }
