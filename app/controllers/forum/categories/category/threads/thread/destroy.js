1bf5ac9f14d80bb6810af3b635be287ffd450159
matteo <matbronk@gmail.com>
2022-10-23 23:42:28 +0200

    fix thread destroy redirects

--- app/controllers/forum/categories/category/threads/thread/destroy.js
+++ app/controllers/forum/categories/category/threads/thread/destroy.js
@@ -1,6 +1,20 @@
 import DestroyController from 'amber-ui/controllers/application/destroy';
+import { action } from '@ember/object';
 
 export default class ForumThreadDestroyController extends DestroyController {
   successTransitionTarget = 'forum.categories.category';
+  cancelTransitionTarget = 'forum.categories.category';
   successMessage = 'Topic verwijderd!';
+
+  @action
+  async destroyModel() {
+    this.successTransitionModel = await this.model.category;
+    super.destroyModel();
+  }
+
+  @action
+  async cancel() {
+    this.cancelTransitionModel = await this.model.category;
+    super.cancel();
+  }
 }
--- app/routes/photo-albums/photo-album/photos.js
+++ app/routes/photo-albums/photo-album/photos.js
@@ -2,5 +2,4 @@ import Route from '@ember/routing/route';
 
 export default class PhotosRoute extends Route {
   breadCrumb = null;
-
 }
