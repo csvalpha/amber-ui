9a3f1c5e262f64fee7bbd84d628483c22fc4cf16
matteo <matbronk@gmail.com>
2022-10-23 23:28:40 +0200

    fix debit destroy redirect

--- app/controllers/debit/transactions/destroy.js
+++ app/controllers/debit/transactions/destroy.js
@@ -2,8 +2,14 @@ import DestroyController from 'amber-ui/controllers/application/destroy';
 import { action } from '@ember/object';
 
 export default class DebitTransactionDestroyController extends DestroyController {
-  successTransitionTarget = 'debit.collections.index';
+  successTransitionTarget = 'debit.collections.show';
   cancelTransitionTarget = 'debit.collections.show';
+
+  @action
+  async destroyModel() {
+    this.successTransitionModel = await this.model.collection;
+    super.destroyModel();
+  }
 
   @action
   async cancel() {
