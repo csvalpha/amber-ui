37727546b116e0d4ecdbbf67a9d53695eb56e66a
guidojw <35309288+guidojw@users.noreply.github.com>
2022-11-01 22:25:27 +0100

    refactor: remove `ember-crumbly` and update BreadCrumbs component

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,18 @@
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
-export default Controller.extend({
-  session: service(),
-  layoutManager: service('layout-manager'),
-});
+export default class ApplicationController extends Controller {
+  @service layoutManager;
+  @service router;
+  @service session;
+
+  get routeInfos() {
+    const linkListToArray = (route) => {
+      return route ? [...linkListToArray(route.parent), route] : [];
+    };
+
+    return linkListToArray(this.router.currentRoute).filter(
+      (routeInfo) => routeInfo.metadata?.breadcrumb
+    );
+  }
+}
