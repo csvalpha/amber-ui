935f744229bd7fe862aee42f2734f367462616a1
matteo <matbronk@gmail.com>
2022-11-19 19:14:12 +0100

    rewrite mail moderation reject

--- app/controllers/mail-moderations/reject.js
+++ app/controllers/mail-moderations/reject.js
@@ -1,24 +1,23 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 import { isInvalidResponse } from 'ember-fetch/errors';
-// todo: octane
-export default Controller.extend({
-  fetch: service(),
-  errorMessage: null,
-  actions: {
-    async submit() {
-      this.set('errorMessage', null);
-      const response = await this.fetch.post(
-        `/stored_mails/${this.model.id}/reject`
-      );
+// todo: flash notice?
+export default class RejectController extends Controller {
+  @service fetch;
+  @tracked errorMessage = null;
+  @action
+  async submit() {
+    this.errorMessage = null;
+    const response = await this.fetch.post(
+      `/stored_mails/${this.model.id}/reject`
+    );
 
-      if (response.ok) {
-        this.model.unloadRecord();
-        this.transitionToRoute('mail-moderations.index');
-      } else if (isInvalidResponse(response)) {
-        const json = await response.json();
-        this.set('errorMessage', json.error);
-      }
-    },
-  },
-});
+    if (response.ok) {
+      this.model.unloadRecord();
+      this.transitionToRoute('mail-moderations.index');
+    } else if (isInvalidResponse(response)) {
+      const json = await response.json();
+      this.errorMessage = json.error;
+    }
+  }
+}
