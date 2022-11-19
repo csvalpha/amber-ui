fdaeab3f8309a28ef85700736a763d293572b0aa
matteo <matbronk@gmail.com>
2022-11-19 14:57:17 +0100

    rewrote print enrolled controller

--- app/controllers/activities/print-enrolled.js
+++ app/controllers/activities/print-enrolled.js
@@ -1,54 +1,64 @@
 import Controller from '@ember/controller';
-import { sort } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  showAllergyInfo: true,
-  showCheckbox: true,
-  currentTab: 'form', // Possible options: [ form, consumptionList ]
-  options: ['Bier', 'Fris'],
-  newOption: '',
-  additionalEmptyRows: 5,
-  showRobertHofstra: true,
+const tabs = ['form', 'consumptionList'];
+const formTab = tabs[0];
+const consumptionListTab = tabs[1];
 
-  sortedString: Object.freeze(['userFullName']),
-  sortedResponses: sort('model.form.responses', 'sortedString'),
+export default class PrintEnrolledController extends Controller {
+  @tracked showAllergyInfo = true;
+  @tracked showCheckbox = true;
+  @tracked currentTab = formTab;
+  @tracked options = ['Bier', 'Fris'];
+  @tracked newOption = '';
+  @tracked showRobertHofstra = true;
+  additionalEmptyRows = 5;
 
-  actions: {
-    print() {
-      const { title } = this.model;
-      const popup = window.open('', 'PRINT', 'height=400,width=600');
+  get sortedResponses() {
+    return this.model.form.get('responses').sortBy('userFullName');
+  }
 
-      popup.document.write(
-        `<html><head><title>Inschrijflijst ${title}</title>`
-      );
-      popup.document.write('<link rel="stylesheet" href="/assets/vendor.css">');
-      popup.document.write(
-        '<link rel="stylesheet" href="/assets/amber-ui.css">'
-      );
-      popup.document.write(
-        '</head><body onload="window.print()" onfocus="setTimeout(window.close, 500)">'
-      );
-      popup.document.write(document.getElementById('printarea').innerHTML);
-      popup.document.write('</body></html>');
-      popup.document.close(); // Necessary for IE >= 10
-      popup.focus(); // Necessary for IE >= 10*/
-    },
-    selectForm() {
-      this.set('currentTab', 'form');
-    },
-    selectConsumptionList() {
-      this.set('currentTab', 'consumptionList');
-    },
-    removeOption(index) {
-      this.options.removeAt(index, 1);
-    },
-    addOption() {
-      const option = this.newOption;
-      this.options.pushObject(option);
-      this.set('newOption', '');
-    },
-    reorderItems(itemModels) {
-      this.set('options', itemModels);
-    },
-  },
-});
+  @action
+  print() {
+    const { title } = this.model;
+    const popup = window.open('', 'PRINT', 'height=400,width=600');
+
+    popup.document.write(`<html><head><title>Inschrijflijst ${title}</title>`);
+    popup.document.write('<link rel="stylesheet" href="/assets/vendor.css">');
+    popup.document.write('<link rel="stylesheet" href="/assets/amber-ui.css">');
+    popup.document.write(
+      '</head><body onload="window.print()" onfocus="setTimeout(window.close, 500)">'
+    );
+    popup.document.write(document.getElementById('printarea').innerHTML);
+    popup.document.write('</body></html>');
+    popup.document.close(); // Necessary for IE >= 10
+    popup.focus(); // Necessary for IE >= 10*/
+  }
+
+  @action
+  selectForm() {
+    this.currentTab = formTab;
+  }
+
+  @action
+  selectConsumptionList() {
+    this.currentTab = consumptionListTab;
+  }
+
+  @action
+  removeOption(index) {
+    this.options.removeAt(index, 1);
+  }
+
+  @action
+  addOption() {
+    this.options.pushObject(this.newOption);
+    this.newOption = '';
+  }
+
+  @action
+  reorderItems(itemModels) {
+    this.options = itemModels;
+  }
+}
--- app/models/form/form.js
+++ app/models/form/form.js
@@ -31,6 +31,10 @@ export default class Form extends Model {
     return this.questions.sortBy('position');
   }
 
+  // get sortedResponses() {
+  //   return this.responses.sortBy('')
+  // }
+
   get currentUserCanRespond() {
     return !this.currentUserResponseCompleted && this.canRespond;
   }
