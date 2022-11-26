686ed90e69fbb6316596dac5363e535662a1b825
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-11-26 16:05:27 +0100

    Fix test

--- app/components/advertisement-tool.js
+++ app/components/advertisement-tool.js
@@ -14,8 +14,9 @@ export default Component.extend({
     }
   ),
 
-  // Don't forget to also increment the 'amountOfAdvertisements' variable
-  // in styles/components/advertisement-tool.scss
+  // Don't forget to also increment the 'amountOfAdvertisements' variable in 
+  // styles/components/advertisement-tool.scss and the number of advertisements 
+  // in tests/rendering/components/advertisement-tool-test.js
   advertisementItems: [
     {
       title: 'Qorting.nl',
