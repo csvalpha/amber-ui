39cbd1dc42ed3e092185208cc7379da4bf106e9f
guidojw <35309288+guidojw@users.noreply.github.com>
2022-11-27 14:31:40 +0100

    test: trigger ESLint

--- app/controllers/books/destroy.js
+++ app/controllers/books/destroy.js
@@ -2,5 +2,5 @@ import DestroyController from 'amber-ui/controllers/application/destroy';
 
 export default class BookDestroyController extends DestroyController {
   successTransitionTarget = 'books.index';
-  cancelTransitionTarget = 'books.show';
+  cancelTransitionTarget = 'books.show'
 }
