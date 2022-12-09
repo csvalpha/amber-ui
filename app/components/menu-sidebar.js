48013915de22d56b8c0bf8cd23ba827925f342a1
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-12-09 14:08:34 +0100

    Change sofia url in menu depending on environment

--- app/components/menu-sidebar.js
+++ app/components/menu-sidebar.js
@@ -1,12 +1,24 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import config from 'amber-ui/config/environment';
 
-export default Component.extend({
-  layoutManager: service('layout-manager'),
-  session: service(),
-  actions: {
-    invalidateSession() {
-      this.session.invalidate();
-    },
-  },
-});
+export default class MenuSidebar extends Component {
+  @service layoutManager;
+  @service session;
+
+  get sofiaUrl() {
+    if (config.environment === 'development') {
+      return 'http://localhost:5000';
+    } else if (config.environment === 'staging') {
+      return 'https://stagingstreep.csvalpha.nl';
+    } else {
+      return 'https://streep.csvalpha.nl';
+    }
+  }
+
+  @action
+  invalidateSession() {
+    this.session.invalidate();
+  }
+}
