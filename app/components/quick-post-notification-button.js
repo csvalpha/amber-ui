bee7899d9462e991b5d6ba4492b49226ad339e3c
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-12-13 12:29:19 +0100

    Lint

--- app/components/quick-post-notification-button.js
+++ app/components/quick-post-notification-button.js
@@ -5,7 +5,7 @@ import Component from '@glimmer/component';
 export default class QuickPostNotificationButton extends Component {
   @service session;
   @service notification;
-  
+
   @action
   sendTest() {
     this.notification.new(
