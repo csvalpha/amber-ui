8aaacd7817cd34b68515ccdc1b32c84516487074
matteo <matbronk@gmail.com>
2022-12-20 18:10:06 +0100

    use ternary operator

--- app/transforms/date-only.js
+++ app/transforms/date-only.js
@@ -1,20 +1,15 @@
 import Transform from '@ember-data/serializer/transform';
 import moment from 'moment';
+import { isNone } from '@ember/utils';
 
 export default class DateOnly extends Transform {
   deserialize(serialized) {
-    if (serialized !== null) {
-      return new Date(serialized);
-    } else {
-      return null;
-    }
+    return isNone(serialized) ? null : new Date(serialized);
   }
 
   serialize(deserialized) {
-    if (deserialized !== null) {
-      return moment(deserialized).format('YYYY-MM-DD');
-    } else {
-      return null;
-    }
+    return isNone(deserialized)
+      ? null
+      : moment(deserialized).format('YYYY-MM-DD');
   }
 }
