38f8168c58a9dcc5fb13e93fa33f6567cd9706d4
Matteo Bronkhorst <32799956+DrumsnChocolate@users.noreply.github.com>
2022-12-21 21:08:05 +0100

    remove old adverts (#688)

--- tests/rendering/components/advertisement-tool-test.js
+++ tests/rendering/components/advertisement-tool-test.js
@@ -13,7 +13,7 @@ module('Integration | Component | advertisement tool', function (hooks) {
 
     assert.equal(
       this.element.querySelectorAll('.advertisement-item').length,
-      52 // Number of advertisements times two
+      34 // Number of advertisements times two
     );
   });
 });
