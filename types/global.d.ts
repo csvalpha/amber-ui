4de1ac0dacd5fe507aea5fa9e8c0707f75bb70c5
guidojw <35309288+guidojw@users.noreply.github.com>
2022-12-31 14:25:58 +0100

    refactor: rename alpha-amber to amber-ui

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module 'alpha-amber/templates/*' {
+declare module 'amber-ui/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
