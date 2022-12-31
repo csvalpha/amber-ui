87f365d254272af031ca67a6718306e36014f569
guidojw <35309288+guidojw@users.noreply.github.com>
2022-12-31 18:36:48 +0100

    chore: fix lint

--- tests/integration/components/model-form/checkbox-group-test.ts
+++ tests/integration/components/model-form/checkbox-group-test.ts
@@ -1,6 +1,4 @@
-import { click, render } from '@ember/test-helpers';
-import { module, test } from 'qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 
 module('Integration | Component | checkbox-group', function (hooks) {
