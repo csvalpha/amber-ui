3f0aeb51b51b64a2fe546df5ab321c9244f0da5a
matteo <matbronk@gmail.com>
2023-01-14 16:13:06 +0100

    fix button spacing, rewrite to glimmer, and change the sorting method. \nthese are attempts at fixing the problems with the qp, they solve part of the problem but it seems that some issue with randomly reordering still remains

--- app/components/quick-post.js
+++ app/components/quick-post.js
@@ -1,187 +1,186 @@
 import { inject as service } from '@ember/service';
 import { htmlSafe } from '@ember/template';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { sort, reads } from '@ember/object/computed';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { WelcomeTextLines, SuggestedEmojis } from 'amber-ui/constants';
 import { convertToUnicode } from 'amber-ui/helpers/convert-to-unicode';
 
-export default Component.extend({
-  session: service(),
-  store: service(),
-  messageBus: service(),
-  notification: service(),
-  abilities: service(),
-  init() {
-    this._super(...arguments);
-    this.set('messages', []);
+export default class QuickPost extends Component {
+  @service session;
+  @service store;
+  @service messageBus;
+  @service notification;
+  @service abilities;
+
+  @tracked newQpMessage = '';
+  @tracked page = 1;
+  @tracked totalPages = 1;
+  @tracked messages = [];
+  maxCharacters = 400;
+  emoticons = SuggestedEmojis;
+
+  get sortedMessages() {
+    return this.messages
+      .map((message) => message) // turn the RecordArray into a js array to allow sorting
+      .sort((a, b) => -('' + a.createdAt).localeCompare(b.createdAt));
+  }
+
+  get currentCharacterCount() {
+    return this.newQpMessage.length;
+  }
+
+  constructor() {
+    super(...arguments);
     if (this.abilities.can('show quickpost-messages')) {
-      this.send('loadMessages', 1);
-      this.send('subscribeToQuickpostMessagesMessageBus');
+      this.loadMessages(1);
+      this.subscribeToQuickpostMessagesMessageBus();
     }
-  },
+  }
 
-  newQpMessage: '',
+  get characterCountPercentage() {
+    return Math.round((this.currentCharacterCount / this.maxCharacters) * 100);
+  }
 
-  page: 1,
-  totalPages: 1,
-  messages: null,
-
-  messagesSort: Object.freeze(['created_at:desc']),
-  sortedMessages: sort('messages', 'messagesSort'),
-
-  maxCharacters: 400,
-  currentCharacterCount: reads('newQpMessage.length'),
-  characterCountPercentage: computed(
-    'currentCharacterCount',
-    'maxCharacters',
-    function () {
-      return Math.round(
-        (this.currentCharacterCount / this.maxCharacters) * 100
-      );
+  get progressBarClass() {
+    const count = this.currentCharacterCount;
+    const max = this.maxCharacters;
+    if (count > max) {
+      return 'danger';
+    } else if (count > 0.8 * max) {
+      return 'warning';
     }
-  ),
+    return 'primary';
+  }
 
-  progressBarClass: computed(
-    'currentCharacterCount',
-    'maxCharacters',
-    function () {
-      const count = this.currentCharacterCount;
-      const max = this.maxCharacters;
-
-      if (count > max) {
-        return 'danger';
-      } else if (count > 0.8 * max) {
-        return 'warning';
-      }
-
-      return 'primary';
-    }
-  ),
-  progressBarStyle: computed('characterCountPercentage', function () {
+  get progressBarStyle() {
     return htmlSafe(`width: ${this.characterCountPercentage}%`);
-  }),
+  }
 
-  tooMuchText: computed('currentCharacterCount', 'maxCharacters', function () {
+  get tooMuchText() {
     return this.currentCharacterCount > this.maxCharacters;
-  }),
+  }
 
-  welcomeText: computed(function () {
+  get welcomeText() {
     return WelcomeTextLines[
       Math.floor(Math.random() * WelcomeTextLines.length)
     ];
-  }),
+  }
 
-  emoticons: SuggestedEmojis,
-
-  showLoaderButton: computed('page', 'totalPages', function () {
+  get showLoaderButton() {
     return this.page < this.totalPages;
-  }),
+  }
 
-  actions: {
-    loadMore() {
-      this.send('loadMessages', this.page + 1);
-    },
+  @action
+  loadMore() {
+    this.send('loadMessages', this.page + 1);
+  }
 
-    koeAan(message) {
-      // The best easter egg
-      if (message === 'KOE AAN!!') {
-        const notificationSound = new Audio('/sounds/cow.ogg');
-        this.notification.set('notificationSound', notificationSound);
-        if (this.notification.isSoundEnabled) {
-          notificationSound.play();
-        }
+  @action
+  koeAan(message) {
+    // The best easter egg
+    if (message === 'KOE AAN!!') {
+      const notificationSound = new Audio('/sounds/cow.ogg');
+      this.notification.set('notificationSound', notificationSound);
+      if (this.notification.isSoundEnabled) {
+        notificationSound.play();
       }
-    },
+    }
+  }
 
-    appendSmileyToText(emoticon) {
-      const currentText = this.newQpMessage;
-      this.set('newQpMessage', htmlSafe((currentText || '') + emoticon));
-      this.element.querySelector('#qp-inputfield').focus();
-    },
+  @action
+  appendSmileyToText(emoticon) {
+    const currentText = this.newQpMessage;
+    this.newQpMessage = htmlSafe((currentText || '') + emoticon);
+    this.element.querySelector('#qp-inputfield').focus();
+  }
 
-    destroyMessage(message) {
-      const answer = confirm(
-        'Weet je zeker dat je dit bericht wilt verwijderen?'
-      ); // eslint-disable-line no-alert
-      if (answer) {
-        message.destroyRecord().then(() => {
-          this.set('messages', []);
-          this.send('loadMessages', 1);
-        });
-      }
-    },
-
-    saveMessage() {
-      const message = this.newQpMessage;
-      if (message && message.length > 0 && !this.tooMuchText) {
-        message.trim().replace(/(\r\n|\n|\r)/gm, '');
-        const unicodeMessage = convertToUnicode(message);
-
-        this.store
-          .createRecord('quickpost-message', { message: unicodeMessage })
-          .save();
-
-        this.send('koeAan', unicodeMessage);
-        this.set('newQpMessage', '');
-      }
-    },
-
-    loadMessages(page) {
-      const pageParams = {};
-      pageParams['page[size]'] = 15;
-      pageParams['page[number]'] = page;
-      pageParams.sort = '-created_at';
-      this.store.query('quickpost-message', pageParams).then((result) => {
-        this.messages.addObjects(result);
-        this.set('totalPages', result.get('meta.page_count'));
+  @action
+  destroyMessage(message) {
+    const answer = confirm(
+      'Weet je zeker dat je dit bericht wilt verwijderen?'
+    ); // eslint-disable-line no-alert
+    if (answer) {
+      message.destroyRecord().then(() => {
+        this.messages = [];
+        this.loadMessages(1);
       });
-      this.set('page', page);
-    },
+    }
+  }
 
-    subscribeToQuickpostMessagesMessageBus() {
-      const channel = '/quickpost_messages';
-      this.messageBus.subscribe(
-        channel,
-        (data) => {
-          const json = JSON.parse(data);
-          const quickpostMessageData = {
-            data: {
-              type: 'quickpost-message',
-              id: json.id,
-              attributes: {
-                message: json.message,
-                createdAt: json.created_at,
-              },
-              relationships: {
-                author: {
-                  data: {
-                    type: 'user',
-                    id: json.author_id,
-                  },
+  @action
+  saveMessage() {
+    const message = this.newQpMessage;
+    if (message && message.length > 0 && !this.tooMuchText) {
+      message.trim().replace(/(\r\n|\n|\r)/gm, '');
+      const unicodeMessage = convertToUnicode(message);
+
+      this.store
+        .createRecord('quickpost-message', { message: unicodeMessage })
+        .save();
+      this.koeAan(unicodeMessage);
+      this.newQpMessage = '';
+    }
+  }
+
+  @action
+  async loadMessages(page) {
+    const pageParams = {};
+    pageParams['page[size]'] = 15;
+    pageParams['page[number]'] = page;
+    pageParams.sort = '-created_at';
+    const result = await this.store.query('quickpost-message', pageParams);
+    this.totalPages = result.get('meta.page_count');
+    this.messages = this.store.peekAll('quickpost-message');
+    this.page = page;
+  }
+
+  @action
+  subscribeToQuickpostMessagesMessageBus() {
+    const channel = '/quickpost_messages';
+    this.messageBus.subscribe(
+      channel,
+      (data) => {
+        const json = JSON.parse(data);
+        const quickpostMessageData = {
+          data: {
+            type: 'quickpost-message',
+            id: json.id,
+            attributes: {
+              message: json.message,
+              createdAt: json.created_at,
+            },
+            relationships: {
+              author: {
+                data: {
+                  type: 'user',
+                  id: json.author_id,
                 },
               },
             },
-          };
-          const quickpost = this.store.push(quickpostMessageData);
-          this.set('messages', this.store.peekAll('quickpostMessage'));
-          this.send('notify', quickpost);
-        },
-        null
-      );
-    },
+          },
+        };
+        console.debug(quickpostMessageData);
+        console.debug(this.store.peekAll('quickpost-message'));
+        const quickpost = this.store.push(quickpostMessageData);
+        console.debug(this.store.peekAll('quickpost-message'));
+        this.notify(quickpost);
+      },
+      null
+    );
+  }
 
-    notify(quickpost) {
-      if (
-        this.notification.isEnabled &&
-        quickpost.get('author.id') !== this.session.currentUser.id
-      ) {
-        this.notification.new(
-          quickpost.get('author.fullName'),
-          quickpost.get('message'),
-          quickpost.get('author.avatarThumbUrlOrDefault')
-        );
-      }
-    },
-  },
-});
+  @action
+  notify(quickpost) {
+    if (
+      this.notification.isEnabled &&
+      quickpost.get('author.id') !== this.session.currentUser.id
+    ) {
+      this.notification.new(
+        quickpost.get('author.fullName'),
+        quickpost.get('message'),
+        quickpost.get('author.avatarThumbUrlOrDefault')
+      );
+    }
+  }
+}
