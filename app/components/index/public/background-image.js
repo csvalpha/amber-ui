c15f7c3c3a8c6a06a7cc7031ea3dc02d10754506
Ellen Wittingen <ellen2000w@gmail.com>
2023-05-08 20:28:15 +0200

    added 3 v's and videos

--- app/components/index/public/background-image.js
+++ app/components/index/public/background-image.js
@@ -5,14 +5,7 @@ import { htmlSafe } from '@ember/template';
 @classNames('index-poster')
 @attributeBindings('style')
 export default class BackgroundImage extends Component {
-  images = [
-    '/images/frontpage/feest.jpg',
-    '/images/frontpage/gala-diner.jpg',
-    '/images/frontpage/gala-groep.jpg',
-    '/images/frontpage/skireis.jpg',
-    '/images/frontpage/vereniging.jpg',
-    '/images/frontpage/vuur.jpg',
-  ];
+  images = ['/images/frontpage/banner.jpg'];
 
   get style() {
     return htmlSafe(`background-image: url(${this.headerImageUrl})`);
