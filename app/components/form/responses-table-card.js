dbd2aeaa390a4f62acbfd0beaac538c343837fb7
Wilco <wilcovanbeijnum@gmail.com>
2023-05-19 16:15:04 +0200

    Add copy usernames button to activity responses table (#768)
    * Add copy usernames button to activity responses table
    
    * Fix lint

--- app/components/form/responses-table-card.js
+++ app/components/form/responses-table-card.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
 
-const FormResponsesTableCardComponent = Component.extend({});
+const FormResponsesTableCardComponent = Component.extend({
+  actions: {
+    copyUsernames() {
+      const usernames = this.form
+        .get('responses')
+        .map((response) => response.get('user.username'))
+        .join('\n');
+
+      navigator.clipboard.writeText(usernames);
+    },
+  },
+});
 
 FormResponsesTableCardComponent.reopenClass({
   positionalParams: ['form'],
