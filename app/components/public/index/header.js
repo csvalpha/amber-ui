eb0ccbb501bf5831762134f0bdcbad8c06f72377
Ellen Wittingen <ellen2000w@gmail.com>
2023-06-03 17:53:24 +0200

    cleanup part 1

---
+++ app/components/public/index/header.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+
+
+export default class Header extends Component {
+  images = ['/images/frontpage/banner.jpg'];
+
+  get headerImageUrl() {
+    return this.images[Math.floor(Math.random() * this.images.length)];
+  }
+}
