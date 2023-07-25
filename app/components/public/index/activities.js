49b1d778b6a11f9de4ffbf1d7e1aff99dfd921f9
Ellen Wittingen <101463776+Ellen-Wittingen@users.noreply.github.com>
2023-07-25 12:53:41 +0200

    Redesign of external website 2023 - Index page (#758)
    * Started on index page
    
    * added 3 v's and videos
    
    * added frontpage images
    
    * added activities and photos
    
    * temp commit
    
    * dynamic columns for calendar and photo-albums
    
    * frontpage done - cleanup still needed
    
    * cleanup part 1
    
    * cleanup part 2
    
    * re-structured language files to split up public website from logged in one
    
    * added text to language files
    
    * merge fixed
    
    * minor renamings
    
    * merge fixed - part 2
    
    * fixed linting errors
    
    * fixed one last issue
    
    * fixed requested changes
    
    * fixed ad heights
    
    * Minor fixes
    
    * added untracked files
    
    ---------
    
    Co-authored-by: guidojw <35309288+guidojw@users.noreply.github.com>

---
+++ app/components/public/index/activities.js
@@ -0,0 +1,59 @@
+import { action } from '@ember/object';
+import { htmlSafe } from '@ember/template';
+import { tracked } from '@glimmer/tracking';
+import Component from '@ember/component';
+
+export default class Activities extends Component {
+  @tracked activities;
+  @tracked doubleActivityColumns;
+
+  @tracked modalTitle = '';
+  @tracked modalText = '';
+  @tracked modalLocation = '';
+  @tracked modalImage = '';
+  @tracked modalIsOpen = false;
+
+  @action
+  openModal(activity) {
+    this.modalTitle = activity.get('title');
+    this.modalText = activity.get('description');
+    this.modalLocation = activity.get('location');
+    this.modalImage = activity.get('coverPhotoUrl');
+    this.modalIsOpen = true;
+  }
+
+  @action
+  closeModal() {
+    this.modalIsOpen = false;
+  }
+
+  get modalImageStyle() {
+    if (this.modalImage) {
+      return htmlSafe(
+        `background-image: url(${this.activity.coverPhotoUrl}); background-size: cover; background-position: center;"`
+      );
+    }
+    return htmlSafe(
+      'background: no-repeat center/40% url(/images/alphalogowhite-large.svg), #ffd218;"'
+    );
+  }
+
+  get activitiesMatrix() {
+    if (this.activities) {
+      const matrix = [];
+      for (
+        let index = 0;
+        index < this.activities.length;
+        index += this.doubleActivityColumns ? 2 : 1
+      ) {
+        const pair = [this.activities.objectAt(index)];
+        if (this.doubleActivityColumns && index + 1 < this.activities.length) {
+          pair.push(this.activities.objectAt(index + 1));
+        }
+        matrix.push(pair);
+      }
+      return matrix;
+    }
+    return [];
+  }
+}
