b1cf259276774ab68aa9cc942515b1aae49871cb
Ellen Wittingen <ellen2000w@gmail.com>
2023-10-21 18:03:33 +0200

    Implemented comments in PR

--- app/components/cards/public-room-advert-card.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-const PublicRoomAdvertCardComponent = Component.extend({});
-
-PublicRoomAdvertCardComponent.reopenClass({
-  positionalParams: ['roomAdvert'],
-});
-
-export default PublicRoomAdvertCardComponent;
--- app/components/cards/room-advert-card.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-const RoomAdvertCardComponent = Component.extend({});
-
-RoomAdvertCardComponent.reopenClass({
-  positionalParams: ['roomAdvert'],
-});
-
-export default RoomAdvertCardComponent;
