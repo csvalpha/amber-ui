0a748428f9163ea6eefd673573aa4ab988834741
Wilco <wilcovanbeijnum@gmail.com>
2024-10-07 20:28:45 +0200

    Add feature of tagging users in photos (#838)
    * Add feature of tagging users in photos
    
    * Only show tag button if any users tagged
    
    * Hide tagging by clicking outside area, add profile link, fix XSS warning
    
    * Bugfixes, lint
    
    * apply suggestions and comments
    
    ---------
    
    Co-authored-by: DrumsnChocolate <matbronk@gmail.com>

--- app/components/photo-albums/photo.js
+++ app/components/photo-albums/photo.js
@@ -1,13 +1,20 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class Photo extends Component {
+  @service abilities;
+
   @tracked
   showExif = false;
 
   get showInfo() {
     return this.args.showInfo ?? true;
+  }
+
+  get showTags() {
+    return this.showInfo && this.abilities.can('show photo-tags');
   }
 
   @action
--- config/icons.js
+++ config/icons.js
@@ -63,6 +63,7 @@ module.exports = function () {
       'sort',
       'suitcase',
       'strikethrough',
+      'tag',
       'thumbtack',
       'trash',
       'upload',
