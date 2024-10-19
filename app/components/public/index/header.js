f6fff65b3a14b1156a589c1e98247166df3198fe
Ellen Wittingen <ellen2000w@gmail.com>
2024-10-19 17:33:02 +0200

    Removed header.js and fixed form not submitting anymore

--- app/components/public/index/header.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class Header extends Component {
-  images = ['/images/public/banner.jpg'];
-
-  get headerImageUrl() {
-    return this.images[Math.floor(Math.random() * this.images.length)];
-  }
-}
