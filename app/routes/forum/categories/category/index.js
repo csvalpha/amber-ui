cfb8428ba3d447c833ed1487937072fa8a3f378c
lodewiges <jamestreeg@gmail.com>
2024-10-27 00:07:15 +0200

    Fixed lint

--- app/routes/forum/categories/category/index.js
+++ app/routes/forum/categories/category/index.js
@@ -1,6 +1,5 @@
 import { AuthenticatedRoute } from 'amber-ui/routes/application/application';
 
-
 export default class CategoryIndexRoute extends AuthenticatedRoute {
   beforeModel() {
     this.transitionTo('forum.categories.category.threads');
