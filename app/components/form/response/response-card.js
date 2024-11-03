3b4e44b461e4f2a2b6db0cf8a794855f6b49d66a
DrumsnChocolate <matbronk@gmail.com>
2024-11-03 23:53:45 +0100

    rewrite response-card in octane

--- app/components/form/response/response-card.js
+++ app/components/form/response/response-card.js
@@ -1,45 +1,31 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-const FormResponseCardComponent = Component.extend({
-  flashNotice: service('flash-notice'),
-  classNames: ['card'],
-  form: null,
-  response: null,
-  errorMessage: null,
-  actions: {
-    submitResponse() {
-      this.response
-        .saveWithAnswers()
-        .then(() => {
-          // The response is the first thing that is saved (in order to save answers), so currently the response is
-          // always 'incomplete'. Furthermore, the form has a field 'amountOfResponses' which should be updated.
-          // We now reload the response and the corresponding form.
-          this.response.reload();
-          this.form.reload();
-          this.flashNotice.sendSuccess('Inschrijving opgeslagen');
-        })
-        .catch((error) => {
-          this.set('errorMessage', error.message);
-          if (
-            error.payload?.errors &&
-            error.payload.errors.isAny(
-              'source.pointer',
-              '/data/attributes/user'
-            )
-          ) {
-            this.set(
-              'errorMessage',
-              'Er is al een response gevonden, probeer eerst te refreshen, zie je dit formulier dan nog? Neem dan contact op met de ict-commissie.'
-            );
-          }
-        });
-    },
-  },
-});
+export default class FormResponseCardComponent extends Component {
+  @service flashNotice;
+  @tracked errorMessage = null;
 
-FormResponseCardComponent.reopenClass({
-  positionalParams: ['form', 'response'],
-});
-
-export default FormResponseCardComponent;
+  @action
+  async submitResponse() {
+    try {
+      await this.args.response.saveWithAnswers();
+      // The response is the first thing that is saved (in order to save answers), so currently the response is
+      // always 'incomplete'. Furthermore, the form has a field 'amountOfResponses' which should be updated.
+      // We now reload the response and the corresponding form.
+      this.args.response.reload();
+      this.args.form.reload();
+      this.flashNotice.sendSuccess('Inschrijving opgeslagen');
+    } catch (error) {
+      this.errorMessage = error.message;
+      if (
+        error.payload?.errors &&
+        error.payload.errors.isAny('source.pointer', '/data/attributes/user')
+      ) {
+        this.errorMessage =
+          'Er is al een response gevonden, probeer eerst te refreshen, zie je dit formulier dan nog? Neem dan contact op met de ict-commissie.';
+      }
+    }
+  }
+}
