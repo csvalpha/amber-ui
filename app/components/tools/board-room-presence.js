9380381e67a2a62497ab203c839e60eec0ca9e91
DrumsnChocolate <matbronk@gmail.com>
2024-11-03 17:21:21 +0100

    rewrite board room presence in octane

--- app/components/tools/board-room-presence.js
+++ app/components/tools/board-room-presence.js
@@ -1,19 +1,22 @@
-import { task, timeout } from 'ember-concurrency';
-import Component from '@ember/component';
-import Ember from 'ember';
-import { computed } from '@ember/object';
-import { equal } from '@ember/object/computed';
+import { dropTask, restartableTask, timeout } from "ember-concurrency";
+import Component from '@glimmer/component';
+// import Ember from 'ember';
+import { action } from '@ember/object';
 import moment from 'moment';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import Ember from 'ember';
 
-const BoardRoomPresence = Component.extend({
-  session: service(),
-  store: service(),
-  abilities: service(),
-  presenceModalIsOpen: false,
-  model: [],
+export default class BoardRoomPresence extends Component {
+  @service session;
+  @service store;
+  @service abilities;
 
-  statusOptions: [
+  @tracked presenceModalIsOpen = false;
+  @tracked model = [];
+  @tracked newCurrentUserPresence = null;
+
+  statusOptions = [
     {
       value: 'present',
       label: 'Aanwezig',
@@ -26,46 +29,52 @@ const BoardRoomPresence = Component.extend({
       value: 'absent',
       label: 'Afwezig',
     },
-  ],
+  ];
 
-  // Periodically poll for new boardroom data
-  poll: task(function* () {
+  @dropTask({ cancelOn: 'didInsertElement' }) *poll() {
     while (!Ember.testing) {
-      this.fetchData.perform();
+      yield this.fetchData.perform();
       yield timeout(1000 * 30); // Wait 30 seconds
     }
-  })
-    .drop()
-    .on('didInsertElement'),
+  }
 
-  // Fetch task is separate from polling task, so we can call it individually.
-  fetchData: task(function* () {
+  @restartableTask *fetchData() {
     /* eslint-disable camelcase */
-    const model = yield this.store.query('board-room-presence', {
+    this.model = yield this.store.query('board-room-presence', {
       filter: { current_and_future: true },
     });
     /* eslint-enable camelcase */
+  }
 
-    this.set('model', model);
-  }).restartable(),
+  constructor() {
+    super(...arguments);
+    this.poll.perform();
+  }
 
-  currentUserPresence: computed('model.[]', function () {
+  get currentUserPresence() {
+    if (this.newCurrentUserPresence) {
+      return this.newCurrentUserPresence;
+    }
     return (
-      this.model.filter((presence) => presence.get('user.isCurrentUser'))[0] ||
+      this.model.filter((presence) => presence.user.get('isCurrentUser'))[0] ||
       null
     );
-  }),
+  }
 
-  sortedPresences: computed('model.[]', function () {
+  set currentUserPresence(presence) {
+    this.newCurrentUserPresence = presence;
+  }
+
+  get sortedPresences() {
     return this.model.sortBy('endTime');
-  }),
+  }
 
-  overallStatus: computed('model.[]', function () {
+  get overallStatus() {
     const currentStatusses = this.model
       .filter((presence) => {
         return moment().isBetween(
-          moment(presence.get('startTime')),
-          moment(presence.get('endTime')),
+          moment(presence.startTime),
+          moment(presence.endTime),
           'minute',
           '[)'
         );
@@ -74,49 +83,50 @@ const BoardRoomPresence = Component.extend({
 
     if (currentStatusses.includes('present')) {
       return 'present';
-    } else if (currentStatusses.includes('busy')) {
+    }
+
+    if (currentStatusses.includes('busy')) {
       return 'busy';
     }
 
     return 'absent';
-  }),
+  }
 
-  saveButtonDisabled: equal('currentUserPresence', null),
+  get saveButtonDisabled() {
+    return this.currentUserPresence === null;
+  }
 
-  actions: {
-    setPresenceModalState(state) {
-      this.set('presenceModalIsOpen', state === 'open');
-    },
+  @action
+  setPresenceModalState(state) {
+    this.presenceModalIsOpen = state === 'open';
+  }
 
-    deletePresence() {
-      this.currentUserPresence.destroyRecord().then(() => {
-        this.set('currentUserPresence', null);
-      });
-    },
+  @action
+  deletePresence() {
+    this.currentUserPresence.destroyRecord().then(() => {
+      this.currentUserPresence = null;
+    });
+  }
 
-    newPresence() {
-      if (this.abilities.can('create board-room-presences')) {
-        const newPresenceObject = this.store.createRecord(
-          'board-room-presence',
-          {
-            startTime: moment().startOf('minute').toDate(),
-            endTime: moment().startOf('minute').add(1, 'hours').toDate(),
-            status: 'present',
-            user: this.session.currentUser,
-          }
-        );
-        this.set('currentUserPresence', newPresenceObject);
-      }
-    },
+  newPresence() {
+    if (this.abilities.can('create board-room-presences')) {
+      this.currentUserPresence = this.store.createRecord(
+        'board-room-presence',
+        {
+          startTime: moment().startOf('minute').toDate(),
+          endTime: moment().startOf('minute').add(1, 'hours').toDate(),
+          status: 'present',
+          user: this.session.currentUser,
+        }
+      );
+    }
+  }
 
-    save() {
-      const presence = this.currentUserPresence;
-      const fetch = this.fetchData;
-
-      presence.save().then(() => fetch.perform());
-      this.set('presenceModalIsOpen', false);
-    },
-  },
-});
-
-export default BoardRoomPresence;
+  @action
+  save() {
+    const presence = this.currentUserPresence;
+    const fetch = this.fetchData;
+    presence.save().then(() => fetch.perform());
+    this.presenceModalIsOpen = false;
+  }
+}
