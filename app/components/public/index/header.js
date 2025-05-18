be09590c7975dd93cf706014bab4afdc0bcb44c7
Lodewiges <jamestreeg@gmail.com>
2025-05-19 01:33:17 +0200

    merge

---
+++ app/components/public/index/header.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import ENV from 'amber-ui/config/environment';
+
+export default class HeaderComponent extends Component {
+
+  get interestedFormLink() {
+    return ENV.APP.formLinkInterested;
+  }
+}
