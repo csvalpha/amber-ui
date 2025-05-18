1760988891bd8b0cdad8cfc0e22c3d275c31773a
Lodewiges <jamestreeg@gmail.com>
2025-05-19 01:35:49 +0200

    fix variable

--- app/components/public/index/header.js
+++ app/components/public/index/header.js
@@ -1,9 +1,9 @@
 import Component from '@glimmer/component';
-import ENV from 'your-app-name/config/environment';
+import ENV from 'amber-ui/config/environment';
 
 export default class HeaderComponent extends Component {
 
   get interestedFormLink() {
-    return ENV.APP.googleFormLinkInterested;
+    return ENV.APP.formLinkInterested;
   }
 }
