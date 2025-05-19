8f93749ed518e04661411469ddeab01c1698161b
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-05-19 02:01:41 +0200

    Update header.js

--- app/components/public/index/header.js
+++ app/components/public/index/header.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import ENV from 'amber-ui/config/environment';
 
 export default class HeaderComponent extends Component {
-
   get interestedFormLink() {
     return ENV.APP.formLinkInterested;
   }
