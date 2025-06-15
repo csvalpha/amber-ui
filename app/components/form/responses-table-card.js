afc04d36025c462e788afdeb16d322b9e490913e
Lodewiges <jamestreeg@gmail.com>
2025-06-16 01:21:15 +0200

    update js file

--- app/components/form/responses-table-card.js
+++ app/components/form/responses-table-card.js
@@ -1,22 +1,26 @@
-import Component from '@glimmer/component';
+import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
-export default class FormResponsesTableCardComponent extends Component {
-  @service session;
-  @tracked showAllergyInfo = false;
-  @action
-  copyUsernames() {
-    let usernames = this.form
-      .get('responses')
-      .map((response) => response.get('user.username'));
-    if (!this.form.currentUserResponseCompleted) {
-      usernames = usernames.filter(
-        (name) => name !== this.session.currentUser.username
-      );
-    }
-    usernames = usernames.join('\n');
-    navigator.clipboard.writeText(usernames);
-  }
-}
+const FormResponsesTableCardComponent = Component.extend({
+  session: service('session'),
+  actions: {
+    copyUsernames() {
+      let usernames = this.form
+        .get('responses')
+        .map((response) => response.get('user.username'));
+      if (!this.form.currentUserResponseCompleted) {
+        usernames = usernames.filter(
+          (name) => name !== this.session.currentUser.username
+        );
+      }
+      usernames = usernames.join('\n');
+      navigator.clipboard.writeText(usernames);
+    },
+  },
+});
+
+FormResponsesTableCardComponent.reopenClass({
+  positionalParams: ['form'],
+});
+
+export default FormResponsesTableCardComponent;
