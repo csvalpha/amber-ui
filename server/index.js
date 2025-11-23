1f439badf6eb2564639b89da6281618507b0e300
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-11-23 15:14:40 +0100

    Remove CORS verification (#1059)
    * Use Sofia.url for CORS configuration
    
    * hardcode CORS
    
    * remove cors entirely

--- server/index.js
+++ server/index.js
@@ -10,30 +10,10 @@
 // };
 
 module.exports = function (app) {
-  const cors = require('cors');
   const globSync = require('glob').sync;
   const mocks = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   const proxies = globSync('./proxies/**/*.js', { cwd: __dirname }).map(
     require
-  );
-
-  // Hardcoded allowed origins for CORS
-  const allowedOrigins = new Set([
-    'http://localhost:5000',
-    'https://stagingstreep.csvalpha.nl',
-    'https://streep.csvalpha.nl',
-  ]);
-
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.has(origin)) return callback(null, true);
-        return callback(new Error('Not allowed by CORS'));
-      },
-      credentials: true,
-    })
   );
 
   // Log proxy requests
